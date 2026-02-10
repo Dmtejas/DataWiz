72c16c48ecf7fca5da547a5217bec7c4f795234b
Dmtejas <tejastejas0770@gmail.com>
2026-02-10 23:40:58 +0530

    fix: Fixed the casing

--- DataWiz/src/pages/AdminLogin.jsx
+++ DataWiz/src/pages/AdminLogin.jsx
@@ -10,7 +10,7 @@ const initialFormData = {
     password: "",
 };
 
-const AdminLogin = () => {
+const adminLogin = () => {
     const { loading, setLoading } = useContext(MyContext);
     const { button, path } = loading;
     const [formData, setFormData] = useState(initialFormData);
@@ -106,4 +106,4 @@ const AdminLogin = () => {
     );
 };
 
-export default AdminLogin;
+export default adminLogin;
